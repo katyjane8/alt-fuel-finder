1a5a78bfb1974e52cf795a6d797cf978bc1b5287
katyjane8 <katy.jane8@gmail.com>
2018-03-28 10:53:13 -0600

    feature test passing

--- spec/features/user_can_search_spec.rb
+++ spec/features/user_can_search_spec.rb
@@ -5,16 +5,19 @@ feature "User can search on the homepage by" do
     visit "/"
 
     fill_in "q", with: 80203
+
     click_on "Locate"
 
     expect(current_path).to eq("/search")
 
     expect(page).to have_content("Station Result Count: 10")
 
-    within(".stations") do
-      expect(".station_name").to have_content("Walgreens")
-      expect(".street_address").to have_content("80925 US Highway ")
-      expect(".fuel_types").to have_content("")
+    within(first(".stations")) do
+      expect(page).to have_content("Walgreens")
+      expect(page).to have_content("80925 US Highway 111")
+      expect(page).to have_content("ELEC")
+      expect(page).to have_content("0.68495")
+      expect(page).to have_content("MO: 12:00am-12:00am")
     end
   end
 end
