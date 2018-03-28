d1120582c8760c8d888a53b215eff7bdeef058ea
katyjane8 <katy.jane8@gmail.com>
2018-03-28 09:45:25 -0600

    stations in view returning results

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,7 +1,8 @@
 class SearchController < ApplicationController
   def index
-    response = Faraday.get("https://developer.nrel.gov/api/alt-fuel-stations/v1/nearest.json?api_key=#{ENV["NREL_API_KEY"]}&location=80203=&fuel_type=ELEC&limit=10")
+    response = Faraday.get("https://developer.nrel.gov/api/alt-fuel-stations/v1/nearest.json?api_key=#{ENV["NREL_API_KEY"]}&location=80203=&fuel_type=ELEC,LPG&limit=10")
     result = parse_json(response)
+    @stations = result["fuel_stations"]
   end
 
   private
